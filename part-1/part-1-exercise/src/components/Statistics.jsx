9a89fb4f3c52e4b23479dc932e87badf11c5647e
ESHWARGEEK <eshwarreddy8382@gmail.com>
2025-12-24 09:59:57 +0530

    finished 1.11 unicafe step 6

--- part-1/part-1-exercise/src/components/Statistics.jsx
+++ part-1/part-1-exercise/src/components/Statistics.jsx
@@ -3,12 +3,12 @@ const Statistics = ({ total, average, percentage, good, bad, neutral }) => {
   if (good + neutral + bad !== 0) {
     return (
       <div>
-        <StatisticLine text="good" value={good} />
-        <StatisticLine text="neutral" value={neutral} />
-        <StatisticLine text="bad" value={bad} />
-        <StatisticLine text="all" value={total} />
-        <StatisticLine text="average" value={average} />
-        <StatisticLine text="positive" value={percentage} />
+        <StatisticLine good="good" good_value={good} />
+        <StatisticLine neutral="neutral" neutral_value={neutral} />
+        <StatisticLine bad="bad" bad_value={bad} />
+        <StatisticLine total="all" total_value={total} />
+        <StatisticLine average="average" average_value={average} />
+        <StatisticLine percentage="positive" percentage_value={percentage} />
       </div>
     );
   } else {
